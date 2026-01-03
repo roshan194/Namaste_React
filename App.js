0f6dbed0ccc7778403f774fcf5144159a85b5114
Roshan <roshan@123>
2026-01-03 17:29:22 +0530

    Ep-2

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
-const Parent = React.createElement("div", {id: "parent"}, 
-    React.createElement("div", {id: "child"},[ 
-        React.createElement("h1", {}, "I am h1 tag inside child div"),
-        React.createElement("h2", {}, "I am h2 tag inside child div")
-    ]), 
-    React.createElement("div", {id: "sibling"},[ 
-        React.createElement("h1", {}, "I am h1 tag inside sibling div"),
-        React.createElement("h2", {}, "I am h2 tag inside sibling div")
-    ])
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Parent = React.createElement(
+  "div",
+  { id: "parent" },
+
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { key: "child-h1" }, "I am h1 tag inside child div"),
+    React.createElement("h2", { key: "child-h2" }, "I am h2 tag inside child div"),
+  ]),
+
+  React.createElement("div", { id: "sibling" }, [
+    React.createElement("h1", { key: "sib-h1" }, "I am h1 tag inside sibling div"),
+    React.createElement("h2", { key: "sib-h2" }, "I am h2 tag inside sibling div"),
+  ])
 );
 
-console.log(Parent);// To see the React element structure in the console (object)
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(Parent);
